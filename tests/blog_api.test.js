a1e2c1732f06d64444c1211f5b23b0db2efe31b9
responderi <rv.mahti@gmail.com>
2019-07-30 11:31:08 +0300

    4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -24,6 +24,31 @@ test('all blogs are returned', async() => {
   expect(response.body.length).toBe(helper.initialBlogs.length)
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'new cool blog',
+    author: 'new',
+    url: 'new',
+    likes: 2
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(r => r.title)
+
+  expect(response.body.length).toBe(helper.initialBlogs.length + 1)
+  expect(contents).toContain(
+    'new cool blog'
+  )
+
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
